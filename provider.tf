0770ad38b83cc30c8bfc8d2bc9018967052afa72
marcos2soares <40725865+marcos2soares@users.noreply.github.com>
2019-07-22 18:19:48 -0300

    Ajuste
    a

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = "${file("<your_service_account.json>")}"
- project     = "@PROJETO@"
- region      = "@REGIAO@"
- zone        = "@ZONA@"
+ credentials = "${file("./cred.json>")}"
+ project     = "${var.project}"
+ region      = "${var.region}"
+ zone        = "${var.zone}"
 }
