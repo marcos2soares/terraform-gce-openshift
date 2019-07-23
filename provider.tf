fdd26ddb635eb3bd7d53f813bcea90a29eaebd62
marcos2soares <40725865+marcos2soares@users.noreply.github.com>
2019-07-22 22:23:42 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = "${file("./credenciais.json>")}"
+ credentials = "${file("./credenciais.json")}"
  project     = "${var.project}"
  region      = "${var.region}"
  zone        = "${var.zone}"
